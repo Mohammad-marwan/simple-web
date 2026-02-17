14c50717b6cf2a11a3876b551ce5a10a352d4d07
Mohammad-marwan <mohammad672001@gmail.com>
2026-02-17 12:46:01 +0300

    Adding a protected router

--- src/components/user/ProtectedRouter/ProtectedRouter.jsx
+++ src/components/user/ProtectedRouter/ProtectedRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
 
-export default function ProtectedRouter() {
+export default function ProtectedRouter({children}) {
   const token = localStorage.getItem('userToken');
     if(!token){
         return <Navigate to="/Login"/>
